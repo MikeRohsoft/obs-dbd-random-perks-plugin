66a4b01174627bf569969e2d8058ef7cd3764c6f
Mike Rohsoft <godlike@cracercrew.de>
2020-12-08 18:07:27 +0100

    Updated to 4.4.0 and added random Killer

---
+++ db/killers.lua
@@ -0,0 +1,200 @@
+return {
+-- Trapper
+    {
+        image = 'storeBackground_TR',
+        lang = { 
+            'The Trapper', 
+            'ツラッパー', 
+            'Der Fallensteller' 
+        } 
+    },
+-- Wraith
+    {
+        image = 'storeBackground_WR',
+        lang = {
+            'The Wraith',
+            'レイス',
+            'Der Geist' 
+        } 
+    },
+-- Billy
+    { 
+        image = 'storeBackground_HB',
+        lang = { 
+            'The Hillbilly', 
+            'ヒッルビッリ', 
+            'Der Hinterwäldler' 
+        } 
+    },
+-- Nurse
+    {
+        image = 'storeBackground_NR',
+        lang = {
+            'The Nurse',
+            'ナース',
+            'Die Krankenschwester'
+        } 
+    },
+-- Huntress
+    {
+        image = 'storeBackground_BE',
+        lang = { 
+            'The Huntress', 
+            'ハントレス', 
+            'Die Jägerin' 
+        } 
+    },
+-- Mayers
+    {
+        image = 'storeBackground_SH',
+        lang = {
+            'The Shape', 
+            'シェイプ', 
+            'Der Wandler' 
+        } 
+    },
+-- Hag
+    {
+        image = 'storeBackground_HA',
+        lang = {
+            'The Hag',
+            'ハグ',
+            'Die Hexe'
+        }
+    },
+-- Doctor
+    {
+        image = 'storeBackground_DO',
+        lang = {
+            'The Doctor',
+            'ドクター',
+            'Der Doktor'
+        }
+    },
+-- Bubba
+    {
+        image = 'storeBackground_CA',
+        lang = {
+            'The Cannibal',
+            'カニバル',
+            'Der Kannibale'
+        }
+    },
+-- Freddy
+    {
+        image = 'storeBackground_SD',
+        lang = {
+            'The Nightmare',
+            'ナイトメア',
+            'Der Alptraum'
+        }
+    },
+-- Pig
+    {
+        image = 'storeBackground_FK',
+        lang = {
+            'The Pig',
+            'ピグ',
+            'Das Schwein'
+        }
+    },
+-- Clown
+    { 
+        image = 'storeBackground_GK',             
+        lang = { 
+            'The Clown', 
+            'クラウン', 
+            'Der Clown' 
+        } 
+    },
+-- Spirit
+    { 
+        image = 'storeBackground_RY',             
+        lang = { 
+            'The Spirit', 
+            'スピリト', 
+            'Das Gespenst' 
+        } 
+    },
+-- Legion
+    {
+        image = 'storeBackground_KK',             
+        lang = { 
+            'The Legion', 
+            'リジョン', 
+            'Die Legion' 
+        } 
+    },
+-- Plague
+    { 
+        image = 'storeBackground_MK',             
+        lang = { 
+            'The Plague', 
+            'プレイグ', 
+            'Die Seuche' 
+        } 
+    },
+-- Ghostface
+    { 
+        image = 'storeBackground_OK',             
+        lang = { 
+            'Ghostface', 
+            'ゴーストフェイス', 
+            'Ghostface' 
+        } 
+    },
+-- Demo
+    { 
+        image = 'Qatar/QK_storeBackground',       
+        lang = { 
+            'The Demogorgon', 
+            'デモゴルゴン', 
+            'Der Demogorgon' 
+        } 
+    },
+-- Oni
+    { 
+        image = 'Sweden/SK_storeBacSweden',       
+        lang = { 
+            'The Oni', 
+            '鬼', 
+            'Der Oni' 
+        } 
+    },
+-- Deathslinger
+    { 
+        image = 'Ukraine/UK_storeBaUkraine',      
+        lang = { 
+            'The Deathslinger', 
+            'デススリンガー', 
+            'Der Todesbote' 
+        } 
+    },
+-- Traffic Cone Head
+    { 
+        image = 'Wales/K20_storeBackground',      
+        lang = { 
+            'The Executioner', 
+            'エクセキューショナー', 
+            'Der Henker' 
+        } 
+    },
+-- Blight
+    { 
+        image = 'Yemen/K21_storeBackground',      
+        lang = { 
+            'The Blight', 
+            'ブライト', 
+            'Die Fäule' 
+        } 
+    },
+-- Baby
+    { 
+        image = 'Aurora/K22_storeBackground',     
+        lang = { 
+            'The Twins', 
+            'ツインズ', 
+            'Die Zwillinge' 
+        } 
+    },
+}
